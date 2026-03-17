8c84e36e8fae0d69cc82c4a0bca9b9b91e0dfc9e
tomseidell <75682748+tomseidell@users.noreply.github.com>
2026-03-17 10:32:18 +0100

    commenting

--- src/train/treshold.py
+++ src/train/treshold.py
@@ -7,6 +7,14 @@ from src.data.data import Data
 import json
 import pandas as pd
 
+"""
+In this file we will perform treshold tuning. With the given problem (Fraud detection of payments), we especially focused
+on optimizing the Recall Metric, as we want to detect as much Fraud payments as possible. However, we also want to keep
+the precision above 50, to not detect too many normal payments as fraud.
+The sweetspot here the treshold 0.0750 (0.585209 Precision, 0.822201 Recall)
+"""
+
+
 # load best parameter according to HyperParameter tuning
 with open(Path(__file__).resolve().parents[2] / "src" / "models" / "best_params.json", "r") as f:
     best_params = json.load(f)
@@ -16,13 +24,14 @@ with open(Path(__file__).resolve().parents[2] / "src" / "models" / "best_params.
 data = Data()
 X_train, X_val, X_test, y_train, y_val, y_test= data.prepare_data()
 
+
 # add params from above and fit the train data
 model = XGBClassifier(**best_params).fit(X_train, y_train)
 
 # call the model on the test set
 y_pred_test = model.predict_proba(X_test)[:, 1]
 
-# Treshold Tuning
+
 tresholds = [0.0125, 0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25, 0.275, 0.3, 0.325, 0.35, 0.375, 0.4, 0.425, 0.45, 0.475, 0.5]
 results = []
 for t in tresholds:
@@ -34,9 +43,9 @@ for t in tresholds:
         "f1": metrics.f1,
         "roc_auc": metrics.roc_auc
     })
-    metrics.print_eval_report(headline=f"Threshold: {t}")
+    #metrics.print_eval_report(headline=f"Threshold: {t}")
 
-# print overview for each treshold
+# print overview for all tresholds
 df_results = pd.DataFrame(results)
 print("\n\n ** Overview **")
 print(df_results.to_string(index=False))
