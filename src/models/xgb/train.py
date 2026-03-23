3733879159c5f80ac8fbee8a949c729426dde91b
tomseidell <75682748+tomseidell@users.noreply.github.com>
2026-03-23 14:50:35 +0100

    perform parameter tuning also for feature engineering. all in pipeline combined

--- src/models/xgb/train.py
+++ src/models/xgb/train.py
@@ -8,18 +8,23 @@ sys.path.insert(0, str(ROOT))
 from src.data.loader import Loader
 from src.data.splitter import split_data
 from src.pipeline.pipeline import build_pipeline
+from src.models.xgb.config import DROP_VARIANTS
 
 PARAMS_PATH = Path(__file__).resolve().parent / "best_params.json"
+PIPELINE_PARAMS = Path(__file__).resolve().parent / "best_pipeline_params.json"
 MODEL_PATH  = Path(__file__).resolve().parent / "pipeline.pkl"
 
 def train():
     with open(PARAMS_PATH, "r") as f:
         params = json.load(f)
 
+    with open(PIPELINE_PARAMS, "r") as f:
+        pipeline_params = json.load(f)
+
     loader = Loader().load()
     X_train, X_test, y_train, y_test = split_data(loader.df)
 
-    pipeline = build_pipeline(params)
+    pipeline = build_pipeline(params, drop_cols=DROP_VARIANTS[pipeline_params["drop_variant"]], number_of_mail_provider=pipeline_params["email_top_n"])
     pipeline.fit(X_train, y_train)
 
     joblib.dump(pipeline, MODEL_PATH) # save model to given path
